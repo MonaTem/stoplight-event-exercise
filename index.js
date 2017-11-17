3170c4ac9267e566834cabce35fc4b4984a3fc3f
Mona Temchin <monatemchin@gmail.com>
2017-11-17 13:30:32 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-(function() {
-  'use strict';
+document.getElementById("controls").addEventListener("click", function(e) {
 
-  // YOUR CODE HERE
-})();
+  const go = "go";
+  let target = e.target.id;
+  let partialID = target.substr(0, 4);
+  let goID = partialID.substr(0, 2);
+
+
+  // e.target is the clicked element!
+
+  if (e.target && e.target.className === "button") {
+
+
+    if (goID === go) {
+      partialID = goID;
+    }
+
+
+    let trafficID = partialID + "Light";
+
+    document.getElementById(trafficID).classList.add(partialID);
+
+
+  }
+
+
+});
